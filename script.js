057667ba7a15d79fd02baabb9bacd8a400f7c2b3
ajardeen <mnazar87540@gmail.com>
2024-10-26 01:28:11 +0530

    add comment line to the code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const cards = document.querySelectorAll(".card");
 const movesElement = document.getElementById("moves");
 const matchedElement = document.getElementById("matched");
@@ -8,18 +7,19 @@ let cardOne,
 let disableDeck = false;
 let moves = 0;
 
+// this matchCard function responsible for finding the matched cards and adding effects to shake and flipcard back to original state if not matched
 function matchCard(cardOneImg, cardTwoImg) {
   if (cardOneImg === cardTwoImg) {
-    matched++;
+    matched++; //matched is increased by 1
     matchedElement.textContent = "Matched: " + matched;
-    if(matched === 6) {
-        stopTimer();
-      }
+    if (matched === 6) { //if matched is 6 then all cards are matched
+      stopTimer();
+    }
     cardOne.removeEventListener("click", flipCard);
     cardTwo.removeEventListener("click", flipCard);
     cardOne = cardTwo = "";
     return (disableDeck = false);
-  } else if (cardOne !== cardTwo) {
+  } else if (cardOne !== cardTwo) { //if cards are not matched flip back to original state
     setTimeout(() => {
       cardOne.classList.add("shake");
       cardTwo.classList.add("shake");
@@ -34,48 +34,46 @@ function matchCard(cardOneImg, cardTwoImg) {
 }
 
 function flipCard(e) {
-  if (time === 0) {
+  if (time === 0) { //if time is 0 then start timer to count how much time is taken to match all cards
     startTimer();
   }
- 
+
   let clickedCard = e.target.parentElement;
-  if (cardOne !== clickedCard && !disableDeck) {
-    moves++;
-    movesElement.innerText = "Moves used: " + moves;
+  if (cardOne !== clickedCard && !disableDeck) {  //if card is not clicked then flip card and add class flip to card
+    moves++;  //moves is increased by 1
+    movesElement.innerText = "Moves used: " + moves; 
     clickedCard.classList.add("flip");
-    if (!cardOne) {
+    if (!cardOne) { //if card is not clicked then flip card and add class flip to card
       return (cardOne = clickedCard);
     }
     cardTwo = clickedCard;
-    disableDeck = true;
+    disableDeck = true;   
     let cardOneImg = cardOne.querySelector(".card-back img").src,
       cardTwoImg = cardTwo.querySelector(".card-back img").src;
     matchCard(cardOneImg, cardTwoImg);
-  
   }
 }
 
-//time function
+//time function to measure how much time is taken to match all cards
 let time = 0;
 let setIntervalTime = null; // Initialize as null
 let timer = document.getElementById("timer");
 function startTimer() {
-// Only start if no interval is running
-if (setIntervalTime === null) {
-  setIntervalTime = setInterval(function () {
+  // Only start if no interval is running
+  if (setIntervalTime === null) {
+    setIntervalTime = setInterval(function () {
       time++;
       timer.innerText = "Time: " + time;
-  }, 1000);
+    }, 1000);
+  }
 }
-}
-
+// stop timer function
 function stopTimer() {
   clearInterval(setIntervalTime);
   setIntervalTime = null; // Reset to null after clearing
 }
 
-
-// shuffle cards function
+// shuffle cards function responsible for shuffling the cards on the load of the page and reset timing
 function shuffleCards() {
   array = generateRandomNumbers();
   cards.forEach((card, index) => {
